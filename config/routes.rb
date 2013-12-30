713fc2b3f12eb488f295d6ad2dca71ac6f3b68e1
iuhoay <iuhoay@gmail.com>
2013-12-30 19:24:16 +0800

    清理不需要的页面

--- config/routes.rb
+++ config/routes.rb
@@ -2,9 +2,6 @@ RailsBootstrap::Application.routes.draw do
 
   root "users#index"
 
-  get "about" => "static_pages#about"
-  get "contact" => "static_pages#contact"
-
   get "signup" => "users#new"
   post "signup" => "users#create"
   get "signin" => "session#new"
